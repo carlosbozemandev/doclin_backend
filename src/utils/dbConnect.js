4938a543907f8b61e6856deee19d602c1f0b840f
Carlos <carlosbozemandev@gmail.com>
2014-01-01 00:00:00 +0500

    update

--- src/utils/dbConnect.js
+++ src/utils/dbConnect.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 import printStatement from "./printStatement.js";
 
+mongoose.set("strictQuery", false);
+
+const DB_OPTIONS = {
+  dbName: process.env.DB_NAME,
+};
+
 const dbConnect = async () => {
-  await mongoose.connect(process.env.DB_URL).then(() => {
-    printStatement("DB Connected");
-  });
+  await mongoose.connect(process.env.DB_CONNECTION, DB_OPTIONS).then(() => {
+    printStatement("DB Connected")
+  }).catch((error)=>{printStatement(error.message)});
 };
 
 export default dbConnect;
